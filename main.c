3f00801773b118ae74d08646f2076c61ab846f18
jcnmsg <joaomatiasgodinho@gmail.com>
2019-02-28 01:38:46 +0000

    New splash screen, refactor, added performant delay

--- main.c
+++ main.c
@@ -6,7 +6,6 @@
 #include "background/backgroundmap.c"
 #include "background/backgroundtiles.c"
 #include "background/logo.c"
-#include "font/fontmap.c"
 
 void resetPlayerPosition(){
     set_sprite_tile(0, 6);
@@ -14,23 +13,25 @@ void resetPlayerPosition(){
 }
 
 void initGameLoop(){
-    font_t minimal; // Declare font variable
-    font_init(); // Initialize font librarty
-    minimal = font_load(font_min); // Load built in font_min
-    font_set(minimal); // Set built in font_min
     set_bkg_data(39, 3, BackgroundTiles); // Sets which background tileset to use, starts on 39, after the font load, counts three tiles
     set_bkg_tiles(0, 0, 20, 18, BackgroundMap); // Sets which background map to use and position on screen starting on x=0, y=0 (offscreen) and spanning 20x18 tiles of 8 pixels each
     set_sprite_data(0, 7, Player); // Sets the player sprite, starts on zero, counts seven
     set_sprite_tile(0, 6); // Sets the tileset used by the player sprite
     move_sprite(0, 84, 80); // Moves sprite to desired position
-    SHOW_BKG; // Draw background
     SHOW_SPRITES; // Draw sprites
+}
+
+void setDelay(UINT8 loops) {
+    UINT8 i;
+    for (i = 0; i < loops; i++){
+        wait_vbl_done();
+    }
 }
 
 void main(){
     int score = 0;
     int state = 0;
-    
+     
     while(state == 0){ // 0: Main Menu 
         set_bkg_palette(0, 1, gbpic_pal);
         set_bkg_data(0x01, gbpic_tiles, gbpic_dat);
@@ -38,13 +39,10 @@ void main(){
         set_bkg_tiles(0, 0, gbpic_cols, gbpic_rows, gbpic_att);
         VBK_REG = 0; // Sets the Video Bank Register. When set to zero, it loads the tile data into the background. When set to one, it loads the palette data into the background. You're supposed to set the palette data before you set the tile data. 
         set_bkg_tiles(0, 0, gbpic_cols, gbpic_rows, gbpic_map);
-
-        SHOW_BKG;
-        enable_interrupts();
-        DISPLAY_ON;
-
-        waitpad(J_START);
-        waitpadup();
+        SHOW_BKG; // Draw background
+        DISPLAY_ON; // Turn on display
+        waitpad(J_START); // Wait for Start Key
+        waitpadup(); // Wait for release
         initGameLoop();
         state = 1;
     }
@@ -76,6 +74,7 @@ void main(){
                 resetPlayerPosition();
                 break;
         }
+        setDelay(2);
     }
 }
 
