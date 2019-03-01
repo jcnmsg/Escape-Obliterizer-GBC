0755412adee2039db0f7dcb3857a36dd4cdef0fa
jcnmsg <joaomatiasgodinho@gmail.com>
2019-03-01 00:27:39 +0000

    Refined movement, added incremental score overtime, font loading is back on

--- main.c
+++ main.c
@@ -2,10 +2,14 @@
 #include <stdio.h> 
 #include <gb/font.h>
 #include <gb/drawing.h>
+#include <gb/console.h>
 #include "sprites/player.c"
 #include "background/backgroundmap.c"
 #include "background/backgroundtiles.c"
 #include "background/logo.c"
+
+int score = 0;
+int state = 0;
 
 void resetPlayerPosition(){
     set_sprite_tile(0, 6);
@@ -27,52 +31,54 @@ void setDelay(UINT8 loops) {
         wait_vbl_done();
     }
 }
+void countScore() {
+    gotoxy(1, 16);
+    printf("%u", score++);
+}
 
 void main(){
-    int score = 0;
-    int state = 0;
-     
     while(state == 0){ // 0: Main Menu 
+        font_t minimal; // Declare font variable
+        
         set_bkg_palette(0, 1, gbpic_pal);
         set_bkg_data(0x01, gbpic_tiles, gbpic_dat);
         VBK_REG = 1; // Sets the Video Bank Register. When set to zero, it loads the tile data into the background. When set to one, it loads the palette data into the background. You're supposed to set the palette data before you set the tile data. 
         set_bkg_tiles(0, 0, gbpic_cols, gbpic_rows, gbpic_att);
-        VBK_REG = 0; // Sets the Video Bank Register. When set to zero, it loads the tile data into the background. When set to one, it loads the palette data into the background. You're supposed to set the palette data before you set the tile data. 
         set_bkg_tiles(0, 0, gbpic_cols, gbpic_rows, gbpic_map);
         SHOW_BKG; // Draw background
         DISPLAY_ON; // Turn on display
         waitpad(J_START); // Wait for Start Key
         waitpadup(); // Wait for release
+        
+        font_init(); // Initialize font library after state change to avoid overwriting bg tiles
+        color(DKGREY, BLACK, SOLID); // Customize colors of font
+        minimal = font_load(font_min); // Load built in font_min
+        font_set(minimal); // Set built in font_min, will be used on displaying the score, only 36 tiles
         initGameLoop();
         state = 1;
     }
 
     while(state == 1){ // 1: Game Loop
+        countScore();
         switch(joypad()) { // Listens for user input
             case J_LEFT:
                 move_sprite(0, 44, 80); // Moves player sprite accordingly
                 set_sprite_tile(0, 3); // Sets the desired sprite tile to animate the eyes
-                waitpadup(); // Waits for the user to release the pressed key
-                resetPlayerPosition(); // Resets the player position back to the center
                 break;
             case J_RIGHT:
                 move_sprite(0, 124, 80); 
                 set_sprite_tile(0, 4);
-                waitpadup();
-                resetPlayerPosition();
                 break;
             case J_UP:
                 move_sprite(0, 84, 40); 
                 set_sprite_tile(0, 4);
-                waitpadup();
-                resetPlayerPosition();
                 break;
             case J_DOWN:
                 move_sprite(0, 84, 120);
                 set_sprite_tile(0, 2); 
-                waitpadup();
-                resetPlayerPosition();
                 break;
+            default: 
+                resetPlayerPosition(); // If no key, resets player to the center
         }
         setDelay(2);
     }
