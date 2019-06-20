af273b330314c8a3321c291bbb13657c7a2287c3
jcnmsg <joaomatiasgodinho@gmail.com>
2019-06-20 22:23:43 +0100

    updated laser blowing sound

--- game/helpers.c
+++ game/helpers.c
@@ -28,11 +28,11 @@ void playSoundFX(UINT8 fx) {
         NR52_REG = 0x80; // Turn on sound registers, setting it to 0x00 turns them off
         NR51_REG = 0x11; // Select channel to use: 0x11 - 1, 0x22 - 2, 0x33 - 3, 0x88 - 4, 0xFF - All
         NR50_REG = 0x77; // Volume, min: 0x00, max: 0x77
-        NR10_REG = 0x1C; // Channel 1, Register 0 => Binary: 00001100
-        NR11_REG = 0xC6; // Channel 1, Register 1 => Binary: 11000110 
-        NR12_REG = 0x73; // Channel 1, Register 2 => Binary: 01110011
-        NR13_REG = 0x00; // Channel 1, Register 3 => Binary: 00000000
-        NR14_REG = 0xC3; // Channel 1, Register 4 => Binary: 11000011 11000111
+        NR10_REG = 4 | (1 << 3) | (2 << 4); 
+        NR11_REG = 1 | (3 << 6); 
+        NR12_REG = 1 | (0 << 3) | (15 << 4); 
+        NR13_REG = 0xFF & 1020; 
+        NR14_REG = 0xC3; 
     }
 
     if (fx == 1) { // Stun sound fx 1 (Frequency: 1936)
